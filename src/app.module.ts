2b1981fe7684f9098f000a4e6f62967a0fc9c699
Alex McKendry <a.mckendry2@gmail.com>
2022-03-09 14:13:30 -0500

    added redirect from get / to get /plants

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlantsModule } from './modules/plants/plants.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [PlantsModule, OrdersModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
